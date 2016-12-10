77eeee9278cb0dd7962d97e98bce57ad4c26fe27
Slawek <yarpiin@gmail.com>
2016-12-10 12:54:03 +0000

    add Lionheart gov
    Change-Id: I1aaa1371ab75da72cd3881d03503c2a10c31f135

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -518,6 +518,9 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #endif
 
 /*********************************************************************
