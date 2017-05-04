76ef3bcaadae9ad9269974e272abe65713fda0dd
villarleg <santialovera20@gmail.com>
2017-05-04 18:02:13 +0200

    Remove picking error

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -524,7 +524,6 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
->>>>>>> b004458... add Bioshock gov
 #endif
 
 /*********************************************************************
